bceb8e1eaf26e052ddbc563f6620be167442592c
MMMJB <michaelbeck445@gmail.com>
2023-07-02 22:11:28 -0400

    implemented react routing

---
+++ src/Pages/Editor/Editor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Editor() {
+    return (
+        <div className="w-10 h-10 bg-blue-500">Editor</div>
+    )
+}
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="w-10 h-10 bg-red-500">Home</div>
+    )
+}
