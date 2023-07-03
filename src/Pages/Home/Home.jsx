429a7147c500e40ed47e106de0ad31bd018d4e23
MMMJB <michaelbeck445@gmail.com>
2023-07-03 16:09:18 -0400

    finished navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Navbar from "../Editor/Components/Navbar";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
     return (
-        <main className="bg-salmon w-full min-h-screen">
+        <div className="bg-salmon w-full min-h-screen">
             <Navbar />
-        </main>
+        </div>
     )
 }
