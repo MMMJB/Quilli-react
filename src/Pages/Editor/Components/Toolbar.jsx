171d69f21a512262bd232f29da01a0d1c32dcebc
MMMJB <michaelbeck445@gmail.com>
2023-07-11 15:00:11 -0400

    finally set up prettier w/ tailwind class sorting

--- src/Pages/Editor/Components/Toolbar.jsx
+++ src/Pages/Editor/Components/Toolbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import EditorToolbarItem from './ToolbarItem'
+import toolbarItems from "../../../Utils/toolbar-items";
+
+import EditorToolbarItem from "./ToolbarItem";
 
 export default function EditorToolbar() {
-    return (
-        <div className="inline-block align-top min-w-[300px] h-0 sticky top-6">
-            <ul className="mr-9">
-                <EditorToolbarItem />
-            </ul>
-        </div>
-    )
+  return (
+    <div className="inline-block align-top min-w-[300px] h-0 sticky top-6">
+      <ul className="mr-9">
+        {toolbarItems.map((item, i) => {
+          return <EditorToolbarItem icon={item.icon} key={i} />;
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/Pages/Editor/Components/ToolbarItem.jsx
+++ src/Pages/Editor/Components/ToolbarItem.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import Icon from "../../../Components/Icon"
+import Icon from "../../../Components/Icon";
 
-export default function EditorToolbarItem() {
-    return (
-        <li className="relative [direction:rtl]">
-            <div className="w-max p-4 bg-white rounded-full shadow-secondary relative my-6 hover:scale-[1.1] transition-all cursor-pointer">
-                <Icon icon={23} size={16} />
-            </div>
-        </li>
-    )
+export default function EditorToolbarItem({ icon, items }) {
+  return (
+    <li className="relative [direction:rtl]">
+      <div className="w-max p-4 bg-white rounded-full shadow-secondary relative my-6 hover:scale-[1.1] transition-all cursor-pointer">
+        <Icon icon={icon} size={16} />
+      </div>
+      <div className="absolute h-[101%] flex items-center top-1/2 -translate-y-1/2 -z-10 mr-6 overflow-x-hidden">
+        <ul className="flex flex-row-reverse items-center min-w-[220px] gap-5 bg-[#FCFCFC] rounded-l-full py-2 px-6 shadow-secondary transition-all duration-[350] ease-in-out"></ul>
+      </div>
+    </li>
+  );
 }
